8484cb17e4cc2e806a145105d12c01c0caa3ca1f
stefl1504 <sad+dev@drylandriver.at>
2024-01-27 23:26:36 +0100

    added poseswaping

---
+++ Assets/Scripts/Player/PoseApplicator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseApplicator : MonoBehaviour
+{
+    public List<AnimationClip> Animations;
+    public int index;
+    public string SwapAxis;
+    public float SwapState;
+    public GameObject ApplicationTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool allow_interaction = SwapState == 0;
+        SwapState = Input.GetAxisRaw(SwapAxis); //should be -1, 0, 1
+        if(allow_interaction)
+        {
+            index += (int)SwapState;
+            if (index == -1)
+                index = Animations.Count - 1;
+            if (index == Animations.Count)
+                index = 0;
+            Apply();
+
+        }
+    }
+
+    public void Apply()
+    {
+        Animations[index].SampleAnimation(ApplicationTarget, 0);
+    }
+}
