783ab24c22a11d2876885426076ec52012f032c3
ldkuba <nawrocki97@gmail.com>
2024-01-27 18:23:41 +0100

    Functional MainMenu and GameManager

--- Assets/Scripts/Scoring/ScoreService.cs
+++ Assets/Scripts/Scoring/ScoreService.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets
 {
@@ -8,10 +10,30 @@ namespace Assets
         public System.Int64 Points;
         public GameObject Prefab;
 
+        private UI.LevelScore m_levelScore;
+
+        [Serializable]
+        public struct Highscore
+        {
+            public System.Int64 Points;
+            public System.Int64 NumJumps;
+            // Whatever else we want to save, maybe per-level highscores?
+            // Maybe name of player or date and time of run?
+        }
+
+        // Storage for user highscores between runs
+        public List<Highscore> Highscores = new List<Highscore>();
+
         private void Awake()
         {
             Points = 0;
+            Highscores.Clear();
         }
+
+        public void ClearPoints() {
+            Points = 0;
+        }
+
         public void AddPoints(System.Int64 NewPoints, Vector3 CollisionPoint)
         {
             Debug.Log("got " + NewPoints + " points");
@@ -24,6 +46,23 @@ namespace Assets
                 if (tmp)
                     tmp.text = NewPoints.ToString();
             }
+
+            // Update current score level
+            UpdateLevelScoreText(Points);
+        }
+
+        public void SetLevelScoreText(UI.LevelScore levelScore)
+        {
+            m_levelScore = levelScore;
+            m_levelScore.SetScore(Points);
+        }
+
+        private void UpdateLevelScoreText(System.Int64 NewPoints)
+        {
+            if (m_levelScore != null)
+                m_levelScore.SetScore(NewPoints);
+            else
+                Debug.LogError("No level score text set");
         }
     }
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+namespace UI {
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
     public Button highscoresButton;
     public Button exitButton;
 
+    public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playButton.onClick.AddListener(Play);
+        highscoresButton.onClick.AddListener(Highscores);
+        exitButton.onClick.AddListener(Exit);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play()
     {
-        
+        gameManager.LaunchGame();
     }
+
+    public void Highscores()
+    {
+        // Debug log
+        Debug.Log("Highscores clicked");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
 }
